adbb5e16b219f22ca09b80e19936e0ba4016307f
bugzinga-z <bugzingaz@gmail.com>
2021-05-13 08:53:29 +0200

    add support images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import "Styles/index.scss";
 import { ReactElement } from "react";
 import Recipes from "./Recipes";
+import sword from "Images/swc-sword.png";
+import swordSvg from "Images/sword.svg";
 
 const App = (): ReactElement => {
   return (
@@ -11,6 +13,8 @@ const App = (): ReactElement => {
           <h1>Hey, React</h1>
         </section>
         <section>
+          <img src={sword as string} alt="sword" width="250" />
+          <img src={swordSvg as string} alt="sword" width="250" />
           <Recipes />
         </section>
       </main>
---
+++ src/types/images.d.ts
@@ -0,0 +1,7 @@
+// src/types/images.d.ts
+declare module "*.jpe";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.png";
+declare module "*.svg";
